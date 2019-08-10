92807c93ffd2105fac67902e300d1acbf0bc9d3b
Tiffany <ong.tifffany1503@gmail.com>
2019-08-10 23:47:12 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,12 +3,12 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  def initialize(adidas)
-  @adidas = adidas
+  def initialize(brand)
+  @brand = brand
   end
 
-  def adidas
-    @adidas
+  def brand
+    @brand
   end
 
 
